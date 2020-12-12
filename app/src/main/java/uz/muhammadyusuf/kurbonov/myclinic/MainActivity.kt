9af4b59454686f2869d33e4e4e3be4b00c34a1a5
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2020-12-13 00:33:33 +0500

    Set up CallReciever Call is being handled --- PERMISSIONS NEEDED ---- Starting database connect

--- app/src/main/java/uz/muhammadyusuf/kurbonov/myclinic/MainActivity.kt
+++ app/src/main/java/uz/muhammadyusuf/kurbonov/myclinic/MainActivity.kt
@@ -1,14 +1,39 @@
 package uz.muhammadyusuf.kurbonov.myclinic
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import uz.muhammadyusuf.kurbonov.myclinic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        if (intent.extras?.containsKey("title") == true)
-            title = intent.extras?.getCharSequence("title")
+        if (intent.extras?.containsKey("title") == true) {
+            val title = intent.extras?.getCharSequence("title")
+            binding.tvNumber.text = title
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), 241)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PERMISSION_GRANTED) {
+            Toast.makeText(this, "Thank you", Toast.LENGTH_SHORT).show()
+        }
     }
 }
