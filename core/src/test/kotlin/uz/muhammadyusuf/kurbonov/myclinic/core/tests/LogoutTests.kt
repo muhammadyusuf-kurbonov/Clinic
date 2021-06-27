d406f65ea542f5c07a67c9304558a275e6c2d790
Androider <muhammadyusuf.kurbonov2002@gmail.com>
2021-06-27 23:35:06 +0500

    logout tests implemented

---
+++ core/src/test/kotlin/uz/muhammadyusuf/kurbonov/myclinic/core/tests/LogoutTests.kt
@@ -0,0 +1,39 @@
+package uz.muhammadyusuf.kurbonov.myclinic.core.tests
+
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import uz.muhammadyusuf.kurbonov.myclinic.core.Action
+import uz.muhammadyusuf.kurbonov.myclinic.core.AppViewModel
+import uz.muhammadyusuf.kurbonov.myclinic.core.SystemFunctionsProvider
+import uz.muhammadyusuf.kurbonov.myclinic.core.states.AuthState
+import uz.muhammadyusuf.kurbonov.myclinic.network.AppRepository
+
+@RunWith(JUnit4::class)
+class LogoutTests {
+    @Test
+    fun `logout - success`() {
+        val repository = mockk<AppRepository>(relaxed = true) {
+        }
+
+        val provider = mockk<SystemFunctionsProvider> {
+            coEvery {
+                writePreference(any(), any())
+            } just Runs
+        }
+
+        runBlocking {
+            val loginViewModel = AppViewModel(this.coroutineContext, provider, repository)
+            loginViewModel.handle(Action.Logout)
+            loginViewModel.authState.assertEmitted {
+                AuthState.AuthRequired == it
+            }
+            coVerify {
+                provider.writePreference("token", "")
+                repository.token = ""
+            }
+        }
+    }
+}
